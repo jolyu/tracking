712aa5c5b4bdac128863a701b2fc687a07955839
sevatm <55739219+sevatm@users.noreply.github.com>
2020-03-09 13:29:07 +0100

    multiObjectTracking

---
+++ multiObjTracking.py
@@ -0,0 +1,30 @@
+import blobDetect as bd
+import trackerFunc as tf
+import cv2
+
+trackerType = "KCF"
+multiTracker = cv2.MultiTracker_create()
+
+bboxes = []
+
+cap = cv2.VideoCapture('vtest.avi')
+#ret, frame = cap.read()
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    success, boxes = multiTracker.update(frame)
+    if cv2.waitKey(0) == ord('s'):
+        newBox = cv2.selectROI('MultiTracker', frame)
+        print(newBox)
+        print(type(newBox))
+        bboxes.append(newBox)
+        multiTracker.add(tf.createTrackerByName(trackerType), frame, newBox)
+    for i, box in enumerate(boxes):
+        p1 = (int(box[0]), int(box[1]))
+        p2 = (int(box[0] + box[2]), int(box[1] + box[3]))
+        cv2.rectangle(frame, p1, p2, (0,0,255), 2, 1)
+    cv2.imshow('MultiTracker', frame)
+    if cv2.waitKey(0) == 27:
+        break
+
+
+cv2.destroyAllWindows()
